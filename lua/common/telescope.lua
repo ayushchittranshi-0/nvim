366565fdb64802751db1a5acc2e446b56cee288b
ayushchittranshi-0 <100466843+ayushchittranshi-0@users.noreply.github.com>
2023-11-14 02:15:40 +0530

    Update telescope.lua

--- lua/common/telescope.lua
+++ lua/common/telescope.lua
@@ -1,10 +1,27 @@
 local cwd = vim.fn.getcwd(-1, -1)
 
 local builtin = require('telescope.builtin')
+local tactions = require('telescope.actions')
+require('telescope').setup{
+    defaults = {
+        mappings = {
+            i = {
+                ["<Tab>"] = require("telescope.actions").move_selection_next,
+                ["<S-Tab>"] = require("telescope.actions").move_selection_previous,
+                ["<C-p>"] = require("telescope.actions").toggle_selection + require("telescope.actions").move_selection_previous,
+                ["<C-n>"] = require("telescope.actions").toggle_selection + require("telescope.actions").move_selection_next,
+            },
+            -- n = {
+            --     ["<c-s>"] = open_in_nvim_tree,
+            -- },
+        },
+    },
+}    
+
 vim.keymap.set('n',  '<C-p>', builtin.find_files, {})
 vim.keymap.set('n','<leader>ff', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fs', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") })
-	end)
+end)
 
 
