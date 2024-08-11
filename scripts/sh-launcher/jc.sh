20c7b2131374bcd213b5202584f2067f08f53fc2
Ayush Chittranshi <ayushchittranshi.0@gmail.com>
2024-08-12 00:44:05 +0530

    Auto commit on 2024-08-12 12:44 AM from maugham-ideapad

--- scripts/sh-launcher/jc.sh
+++ scripts/sh-launcher/jc.sh
@@ -1,2 +1,2 @@
 qdbus org.kde.KWin /KWin setCurrentDesktop 1;
-google-chrome "chat.openai.com" --start-maximized --new-window --window-name="jc";
+google-chrome "chat.openai.com" "claude.ai" --start-maximized --new-window --window-name="jc";
