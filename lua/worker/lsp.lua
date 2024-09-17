04859f5f8de4c84e92b09e277feeaf4e0132d3e4
Ayush Chittranshi <ayushchittranshi.0@gmail.com>
2024-09-18 00:21:45 +0530

    Added new config for ts_ls

--- lua/worker/lsp.lua
+++ lua/worker/lsp.lua
@@ -19,9 +19,9 @@ lsp_zero.on_attach(function(client, bufnr)
     vim.keymap.set('n', '<C-h>', vim.lsp.buf.signature_help, opts)
 end)
 require'lspconfig'.eslint.setup{}
+require'lspconfig'.ts_ls.setup{}
 require'lspconfig'.html.setup{}
 require'lspconfig'.tailwindcss.setup{}
-require'lspconfig'.tsserver.setup{}
 require'lspconfig'.cssls.setup{}
 require'lspconfig'.cssmodules_ls.setup{}
 require'lspconfig'.somesass_ls.setup{}
@@ -124,16 +124,6 @@ cmp.setup.cmdline(':', {
             }
         })
 })
---eslint basic formatting works with this
--- lsp_zero.format_on_save({
---   format_opts = {
---     async = false,
---     timeout_ms = 10000,
---   },
---   servers = {
---     ['tsserver'] = {'javascript', 'typescript'},
---   }
--- })
 
 local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
@@ -184,9 +174,6 @@ cmp.setup({
     },
     },
 })
-
-
-
 --format lsp
 require("conform").setup({
     formatters_by_ft = {
@@ -215,91 +202,3 @@ vim.api.nvim_create_user_command("Wf", function(args)
 end, { range = true })
 
 vim.api.nvim_set_keymap('n', '<leader>pr', ':Wf<CR>', { noremap = true, silent = true })
---maybe formatting working due to eslint config check later
--- local lsp_zero = require('lsp-zero')
-
--- lsp_zero.on_attach(function(client, bufnr)
---   lsp_zero.default_keymaps({buffer = bufnr})
---   local opts = {buffer = bufnr}
-
---   vim.keymap.set({'n', 'x'}, 'gq', function()
---     vim.lsp.buf.format({async = false, timeout_ms = 10000})
---   end, opts)
--- end)
-
-
-
-
-
-
-
-
-
-
-
-
-
- --OLD MAPPING (NOT NEEDED JUST IN CASE)
-
- -- local lsp = require("lsp-zero")
-
- -- lsp.preset("recommended")
-
- -- lsp.ensure_installed({
- --   'tsserver',
- --   'rust_analyzer',
- -- })
-
- -- -- Fix Undefined global 'vim'
- -- lsp.nvim_workspace()
-
-
- -- local cmp = require('cmp')
- -- local cmp_select = {behavior = cmp.SelectBehavior.Select}
- -- local cmp_mappings = lsp.defaults.cmp_mappings({
- --   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
- --   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
- --   ['<CR>'] = cmp.mapping.confirm({ select = true }),
- --   ["<C-Space>"] = cmp.mapping.complete(),
- -- })
-
- -- cmp_mappings['<Tab>'] = nil
- -- cmp_mappings['<S-Tab>'] = nil
-
- -- lsp.setup_nvim_cmp({
- --   mapping = cmp_mappings
- -- })
-
- -- lsp.set_preferences({
- --     suggest_lsp_servers = false,
- --     sign_icons = {
- --         error = 'E',
- --         warn = 'W',
- --         hint = 'H',
- --         info = 'I'
- --     }
- -- })
-
- -- lsp.on_attach(function(client, bufnr)
- --   local opts = {buffer = bufnr, remap = false}
-
- --   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
- --   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
- --   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
- --   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
- --   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
- --   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
- --   vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
- --   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
- --   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
- --   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
- -- end)
-
- -- lsp.setup()
-
- -- vim.diagnostic.config({
- --     virtual_text = true
- -- })
- -- require('luasnip.loaders.from_vscode').lazy_load()
-
-
